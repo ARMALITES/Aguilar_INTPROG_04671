eac8b85e286d1ba3f306539b58f70647e857af10
unknown <UU634@ccs.local>
2024-02-24 05:02:09 +0800

    BACKUP
    BACKUP

---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,15 @@
+﻿namespace PartyInvites.Models
+{
+    public static class Repository
+    {
+        private static List<GuestInvite> response = new List<GuestInvite>();
+
+        public static IEnumerable<GuestInvite> Response { get { return response; } }
+
+        public static void AddResponse(GuestInvite responses)
+        {
+            Console.WriteLine(Response);
+            response.Add(responses);
+        }
+    }
+}
---
+++ PartyInvites/Models/YesInvites.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations
+namespace PartyInvites.Models
+{
+    public class GuestInvite
+    {
+        [Required(ErrorMessage = "Please enter Name.")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage ="Please input email address.")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "Please input correct phone/cell number.")]
+        public string? Phone { get; set; }
+        [Required(ErrorMessage = "Please specified if you will attend.")]
+        public string? WillAttend { get; set; }
+    }      
+}
